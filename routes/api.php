aafe0a650d8784b375718d16b45a134bc218d21c
javier lopez <javierernestomena07@gmail.com>
2022-12-13 22:00:11 -0600

    firtsTables: tokens, message, galleries

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Http\Request;
+
+use App\Http\Controllers\messageController;
+use App\Http\Controllers\tokenController;
+use App\Http\Controllers\GalleryController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +22,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+route::get('/messages',[messageController ::class,'index']);
+route::post('/messages', [messageController::class,'store']);
+route::put('/messages/{id}', [messageController::class,'update']);
+route::delete('/messages/{id}', [messageController::class,'destroy']);
+
+route::get('/tokens',[tokenController ::class,'index']);
+route::post('/tokens', [tokenController::class,'store']);
+route::put('/tokens/{id}', [tokenController::class,'update']);
+route::delete('/tokens/{id}', [tokenController::class,'destroy']);
+
+route::get('/Galleries',[GalleryController ::class,'index']);
+route::post('/Galleries', [GalleryController::class,'store']);
+route::put('/Galleries/{id}', [GalleryController::class,'update']);
+route::delete('/Galerries/{id}', [GalleryController::class,'destroy']);
+
